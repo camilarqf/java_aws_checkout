0b8fa5dd5ad0ca39a41c631a5a9c08183a2f438a
Camila Fonseca <camilarqf@gmail.com>
2022-09-16 15:10:55 -0300

    feature: hibernate validation pedido

--- mscompra/src/main/java/com/mscompra/mscompra/controller/PedidoController.java
+++ mscompra/src/main/java/com/mscompra/mscompra/controller/PedidoController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/pedido")
 public class PedidoController {
@@ -17,7 +19,7 @@ public class PedidoController {
     PedidoService pedidoService;
 
     @PostMapping
-    public ResponseEntity<Pedido> salvar (@RequestBody Pedido pedido){
+    public ResponseEntity<Pedido> salvar (@RequestBody @Valid Pedido pedido){
         return ResponseEntity.ok(pedidoService.salvar(pedido));
     }
 }
